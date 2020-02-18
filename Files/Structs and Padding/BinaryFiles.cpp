d97b39fc399d61c3ed7dc6d9af2bbffceeec9a55
Justin Licari <justinlicari@Justins-MacBook-Air.local>
2020-02-17 21:33:23 -0500

    structs and binary files

--- Files/Structs and Padding/BinaryFiles.cpp
+++ Files/Structs and Padding/BinaryFiles.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
-
+#include <fstream>;
 using namespace std;
+
+// This is useful for saving and opening files in some application.
 
 // Only difference between a struct and a class is that the members of a
 // struct are public by default.
@@ -32,6 +34,56 @@ struct Person {
 #pragma pack(pop)
 
 int main() {
+
+    // Initializes the struct
+    Person someone = { "Frodo", 220, 0.8};
+
+    string filename = "test";
+
+
+    ////// WRITE BINARY FILE ///////
+
+    ofstream outputFile;
+
+    // Need ios::binary so c++ knows it's dealing with a binary file
+    outputFile.open(filename, ios::binary);
+
+    if (outputFile.is_open()) {
+
+
+        // (char *)&someone casts the data stored at the struct instance a a char pointer
+        // Old way of casting to a char pointer:
+        // outputFile.write((char *)&someone, sizeof(Person));
+
+        // New way of casting between pointers:
+        outputFile.write(reinterpret_cast<char *>(&someone), sizeof(Person));
+
+        outputFile.close();
+    } else {
+        cout << "couldn't create file " + filename;
+    }
+
+
+    ////// READ BINARY FILE //////
+
+    Person someoneElse = {};
+
+    ifstream inputFile;
+    
+    inputFile.open(filename, ios::binary);
+
+    if (inputFile.is_open()) {
+
+        // Read the newly generated binary file into
+        // the empty struct instance 'someoneElse'
+        inputFile.read(reinterpret_cast<char *>(&someoneElse), sizeof(Person));
+
+        inputFile.close();
+    } else {
+        cout << "couldn't read file " + filename;
+    }
+
+    cout << someoneElse.name << ", " << someoneElse.age << ", " << someoneElse.weight << endl;
 
     // sizeof() will give you the size in bytes of the object
     // we can see that regardless of the variables we declare in the struct,
