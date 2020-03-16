21c54823140916e8e347de10bb7938214deeb249
Justin Licari <justinlicari@Justins-MacBook-Air.local>
2020-03-16 18:22:12 -0400

    Dequeue

---
+++ The Standard Template Library/Dequeue.cpp
@@ -0,0 +1,5 @@
+// A dequeue is a doubly-ended queue.
+// It's very similar to a vector, but you can add elements to the beginning or the end.
+// So, it's not as efficient as a vector, but you get more flexibility.
+// So, rather than just a push_back, there's also a push_front, as well as a
+// pop_back and pop_front.
