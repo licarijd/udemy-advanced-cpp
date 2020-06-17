51c7710d22a6c7f367b22384fa18a66314a31b2f
Justin Licari <justinlicari@Justins-MacBook-Air.local>
2020-06-17 16:31:57 -0400

    Static casts

--- C++ 11 Features/Casting/StaticCast.cpp
+++ C++ 11 Features/Casting/StaticCast.cpp
@@ -2,7 +2,10 @@
 using namespace std;
 
 class Parent {
-
+public:
+    void speak() {
+        cout << "parent!" << endl;
+    }
 };
 
 class Brother: public Parent {
@@ -53,6 +56,57 @@ int main() {
 
     // This works, but it's dangerous because things can go wrong with virtual 
     // functions. Or we could have methods in Brother that don't exist in Parent.
+    // We could try calling them using this pointer, and the program would just crash.
+
+    // So usually, we'll want to avoid this sort of thing. But sometimes, we may actually
+    // want to do this.
+
+    Parent *ppb = &brother;
+
+    // This is a typical case where we use a child where a parent is expected; Polymorphism 
+    // garuntees us that this will work.
+
+    // Brother *pbb = &brother;
+
+    // Obviously, this will work as well.
+
+    // And, it does make sense in this situation to point it at the same address as *ppb.
+    
+    // The line declaring pbb doesn't compile. We can use a static cast to get it to work.
+
+    Brother *pbb = static_cast<Brother *>(pbb);
+
+    // This can potentially be very error prone. static_cast is purely a compile time thing.
+    // There's no runtime checking. So, the ppb pointer could point at a Parent object and
+    // not at a Brother object. It cuold turn out to be invalid later in the code, calling
+    // Brother methods which don't exist in Parent. 
+
+    // We'll look at a better way to do this with dynamic casts in the next tutorial.
+
+    // Static casts can be used to get your code working, but it's up to you to decide if 
+    // what the code is doing actually makes sense.
+
+    // One other case where you might use static casts (which has a bearing on perfect forwarding)
+    // is:
+
+    Parent &&p = Parent();
+
+    // We're setting an r-value reference to an r-value. This will work with C++ 11.
+
+    // But, what if we wanted to set this r-value reference to an l-value?
+
+    // Parent &&p = parent;
+
+    // This could work, since the objects are of the right type. But it doesn't. We need to use
+    // a static cast to make this work.
+
+    Parent &&p = static_cast<Parent &&>(parent);
+    p.speak();
+
+    // This can be useful if for example you have a function with a parameter type that's an r-value reference,
+    // and for some reason, you need to pass an l-value to that function.
+
+    cout << pbb << endl;
 
     return 0;
 }
